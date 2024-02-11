d9227f9e642cd76cf88b3f22359e9623a2905ad1
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2024-02-11 14:09:22 -0600

    navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,13 @@ import { links } from '../data/dummy'
 import { useStateContext } from '../contexts/ContextProvider'
 
 const Sidebar = () => {
-  const {activeMenu, setActiveMenu} = useStateContext()
+  const {activeMenu, setActiveMenu, screenSize} = useStateContext()
+
+  const handleCloseSidebar = () => {
+    if(activeMenu && screenSize <= 900 ){
+      setActiveMenu(false)
+    }
+  }
 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md md-2 '
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2'
@@ -17,7 +23,7 @@ const Sidebar = () => {
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       {activeMenu && (<>
         <div className='flex justify-between items-center'>
-          <Link to='/' onClick={() => setActiveMenu(false)} className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'>
+          <Link to='/' onClick={handleCloseSidebar} className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'>
             <SiShopware /> <span>Shoppy</span>
           </Link>
 
@@ -34,7 +40,7 @@ const Sidebar = () => {
               <p className='text-gray-400 m-3 mt-4 uppercase'>{item.title}</p>
               {item.links.map(link => (
                 <NavLink 
-                  to={`/${link.name}`} key={link.name} onClick={() => {}}
+                  to={`/${link.name}`} key={link.name} onClick={handleCloseSidebar}
                   className={({ isActive }) => isActive ? activeLink : normalLink}
                 >
                   {link.icon} <span className='capitalize'>{link.name}</span>
